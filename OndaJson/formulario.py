701a25d9c26428bfe2b35c7389231c05f5d7a4e0
Carlos Alvarado <cmalvarado@ufm.edu>
2020-10-29 11:00:24 -0600

    agregar código

---
+++ OndaJson/formulario.py
@@ -0,0 +1,28 @@
+from flask import Flask, url_for, request, redirect
+from jinja2 import Template, Environment, FileSystemLoader
+import json
+File_loader = FileSystemLoader("templates")
+env = Environment(loader=File_loader)
+app = Flask(__name__)
+with open("form.json") as json_file:
+    myjson = json.load(json_file)
+@app.route('/form', methods=["GET", "POST"])
+def form():
+    if request.method == 'POST':
+        _id = request.form['id']
+        _name = request.form['name']
+        _age = request.form['age']
+        _uni = request.form['uni']
+
+        myjson['data'].append({"id":_id, "name":_name,"age":_age,"uni":_uni})
+        return redirect('index.html')
+    template = env.get_template('form.html')
+    return template.render()
+
+@app.route('/')
+def index():
+    template = env.get_template('index.html')
+    return template.render(my_json=myjson['data'], headers=myjson['headers'])
+
+if __name__ == '__main__':
+    app.run()
